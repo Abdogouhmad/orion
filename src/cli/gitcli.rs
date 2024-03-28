e9e314c8a28576701fa31a285bbe016994d68a7e
div-styl <gouhmada@gmail.com>
2024-03-28 00:51:09 +0000

    push subcommand is ready to use now

--- src/cli/gitcli.rs
+++ src/cli/gitcli.rs
@@ -1,7 +1,7 @@
 use crate::Sys;
 use clap::Parser;
 use commandcrafter::{color::Col, execute::Execute};
-use inquire::Text;
+use inquire::{InquireError, Select, Text};
 
 #[derive(Parser, Debug)]
 pub enum Commands {
@@ -38,7 +38,6 @@ impl Commands {
     /// whispercli clone -u=username -r=reponame
     /// whispercli push
     /// ```
-
     pub fn git_cli() {
         let args = Sys::parse();
         if let Some(command) = args.command {
@@ -72,49 +71,100 @@ impl Commands {
                 }
 
                 Commands::Push => {
-                    let commit = Text::new("Please Enter Commit name 😎:").prompt();
-                    match commit {
-                        Ok(commit) => {
-                            // track the changes :)
-                            let add_result = Execute::run("git", &["add", "."]);
-                            if let Err(err) = add_result {
-                                eprintln!("Error adding changes: {:?}", err);
-                                std::process::exit(1);
-                            }
+                    let variety_commits = vec![
+                        "New Improvement to the code base 🚀",
+                        "Bug is Fix 🐛",
+                        "Docs are updated 📚",
+                        "Code is styled 🫠",
+                        "Codebase is refactored 🏭",
+                        "Test is updated 🤖",
+                        "Other changes🙂",
+                        "Customized Commit 😎",
+                    ];
+                    // select option
+                    let selected_commit =
+                        Select::new("Select a commit type", variety_commits).prompt();
 
-                            // commit the changes
-                            let commit_result = Execute::run("git", &["commit", "-m", &commit]);
-                            if let Err(err) = commit_result {
-                                eprintln!("Error committing changes: {:?}", err);
-                                std::process::exit(1);
+                    match selected_commit {
+                        Ok(commit_type) => match commit_type {
+                            "Customized Commit 😎" => {
+                                let customize_commit =
+                                    Text::new("Please Enter Commit Messege 😎:").prompt();
+                                Commands::push_changes(&customize_commit)
                             }
-
-                            // get the branch the name :/
-                            let branch_result =
-                                Execute::run("git", &["rev-parse", "--abbrev-ref", "HEAD"]);
-                            let branch_name = match branch_result {
-                                Ok(bytes) => String::from_utf8_lossy(&bytes).trim().to_string(),
-                                Err(err) => {
-                                    eprintln!("Error getting branch name: {:?}", err);
-                                    std::process::exit(1);
-                                }
-                            };
-
-                            // push the branch head name :')
-                            let push_result = Execute::run(
-                                "git",
-                                &["push", "--set-upstream", "origin", &branch_name],
-                            );
-                            if push_result.is_err() {
-                                eprintln!("Error pushing changes");
-                                std::process::exit(1);
-                            }
-                            println!("{}", Col::print_col(&Col::Magenta, "Code is pushed"));
-                        }
-                        Err(e) => println!("{e}"),
+                            _ => Commands::push_changes(&Ok(commit_type.to_string())),
+                        },
+                        Err(e) => eprintln!("Error: {}", e),
                     }
                 }
             }
+        }
+    }
+
+    /// Pushes the changes represented by the given commit to the remote repository.
+    ///
+    /// # Arguments
+    ///
+    /// * `commit` - A reference to a `Result` containing a `String` representing the commit message or an error.
+    ///
+    /// # Errors
+    ///
+    /// This function will panic and exit the process with a status code of `1` if any of the following errors occur:
+    ///
+    /// * Failed to add changes using `git add .`.
+    /// * Failed to commit changes using `git commit -m <commit_message>`.
+    /// * Failed to determine the current branch name using `git rev-parse --abbrev-ref HEAD`.
+    /// * Failed to push changes to the remote repository using `git push --set-upstream origin <branch_name>`.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if any of the above errors occur.
+    ///
+    /// # Remarks
+    ///
+    /// This function assumes that `git` is installed and accessible in the system's PATH.
+    ///
+    /// # Safety
+    ///
+    /// This function does not pose any safety concerns.
+    ///
+    fn push_changes(commit: &Result<String, InquireError>) {
+        match commit {
+            Ok(commit) => {
+                // track the changes :)
+                let add_result = Execute::run("git", &["add", "."]);
+                if let Err(err) = add_result {
+                    eprintln!("Error adding changes: {:?}", err);
+                    std::process::exit(1);
+                }
+
+                // commit the changes
+                let commit_result = Execute::run("git", &["commit", "-m", &commit]);
+                if let Err(err) = commit_result {
+                    eprintln!("Error committing changes: {:?}", err);
+                    std::process::exit(1);
+                }
+
+                // get the branch the name :/
+                let branch_result = Execute::run("git", &["rev-parse", "--abbrev-ref", "HEAD"]);
+                let branch_name = match branch_result {
+                    Ok(bytes) => String::from_utf8_lossy(&bytes).trim().to_string(),
+                    Err(err) => {
+                        eprintln!("Error getting branch name: {:?}", err);
+                        std::process::exit(1);
+                    }
+                };
+
+                // push the branch head name :')
+                let push_result =
+                    Execute::run("git", &["push", "--set-upstream", "origin", &branch_name]);
+                if push_result.is_err() {
+                    eprintln!("Error pushing changes");
+                    std::process::exit(1);
+                }
+                println!("{}", Col::print_col(&Col::Magenta, "Code is pushed"));
+            }
+            Err(e) => println!("{e}"),
         }
     }
 }
