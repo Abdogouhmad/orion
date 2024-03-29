ad5498006fdede3c13fe416c1aa5f73631a1afa6
div-styl <gouhmada@gmail.com>
2024-03-29 00:03:55 +0000

    Other changes🙂

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+NC='\033[0m'
+
+printcl() {
+    local color=$1
+    local message=$2
+    echo -e "${color}${message}${NC}"
+}
+
+printcl "${YELLOW}" "Installing The CLI into your /usr/bin..."
+sudo curl -L https://github.com/div-styl/whispercli/releases/download/2.2.1/whispercli -o /usr/bin/whispercli
+printcl "${GREEN}" "Installed Successfully!"
