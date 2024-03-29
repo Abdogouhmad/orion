4b9e5fc8bdd1505c969a048d4b82ffdf95b46018
div-styl <gouhmada@gmail.com>
2024-03-29 17:00:45 +0000

    Update!

--- install.sh
+++ install.sh
@@ -10,5 +10,5 @@ printcl() {
 }
 
 printcl "${YELLOW}" "Installing The CLI into your /usr/bin..."
-sudo curl -L https://github.com/div-styl/whispercli/releases/download/2.2.1/whispercli -o /usr/bin/whispercli
+sudo curl -L https://github.com/div-styl/whispercli/releases/download/2.2.1/orion -o /usr/bin/orion
 printcl "${GREEN}" "Installed Successfully!"
