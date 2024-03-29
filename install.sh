05840f2001411b91f901a6d217c7b7aa897175d4
div-styl <gouhmada@gmail.com>
2024-03-29 23:00:50 +0000

    New Improvement to the code base 🚀

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/bash
+#!/bin/bash
+
 GREEN='\033[0;32m'
 YELLOW='\033[0;33m'
 NC='\033[0m'
@@ -9,7 +10,35 @@ printcl() {
     echo -e "${color}${message}${NC}"
 }
 
-printcl "${YELLOW}" "Installing The CLI into your /usr/bin..."
-sudo curl -L https://github.com/div-styl/orion/releases/download/2.2.3-beta.1/orion -o /usr/bin/orion
-sudo chmod +x /usr/bin/orion
-printcl "${GREEN}" "Installed Successfully! run the CLI as command orion -h"
+install_orion() {
+    printcl "${YELLOW}" "Installing The CLI into /usr/bin..."
+    sudo curl -L https://github.com/div-styl/orion/releases/download/2.2.3-beta.1/orion -o /usr/bin/orion
+    sudo chmod +x /usr/bin/orion
+    printcl "${GREEN}" "Installed Successfully! Run the CLI with the command: orion -h"
+}
+
+uninstall_orion() {
+    printcl "${YELLOW}" "Uninstalling The CLI from /usr/bin..."
+    sudo rm /usr/bin/orion
+    printcl "${GREEN}" "Uninstalled Successfully!"
+}
+
+printcl "${YELLOW}" "Choose an action:"
+printcl "${YELLOW}" "1: Install"
+printcl "${YELLOW}" "2: Uninstall"
+read -p "Enter your choice: " choice
+
+case "$choice" in
+    1)
+        install_orion
+        ;;
+    2)
+        uninstall_orion
+        ;;
+    *)
+        printcl "${YELLOW}" "Invalid choice. Please choose 1 to install or 2 to uninstall."
+        exit 1
+        ;;
+esac
+
+exit 0
