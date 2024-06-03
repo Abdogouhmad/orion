f550be355e45d4e2ac4eb4ff2de40c998b204d20
Abdogouhmad <gouhmada@gmail.com>
2024-06-03 23:05:39 +0100

    Codebase is refactored 🏭

--- orion_lib/src/github/gittool.rs
+++ orion_lib/src/github/gittool.rs
@@ -110,32 +110,6 @@ impl GitTool {
                 }
             }
         }
-        // let depth = Text::new("Enter the depth of cloning: ")
-        //     .with_default("0")
-        //     .prompt_skippable();
-
-        // if let (Ok(username), Ok(repo), Ok(depth)) = (username, repo, depth) {
-        //     let clonefmt = format!("git@github.com:{}/{}.git", username, repo);
-        //     let mut clone_pattern = vec!["clone"];
-
-        //     // Only add depth option if depth is provided and greater than 0
-        //     if let Some(depth) = depth.as_deref() {
-        //         let depth_int: usize = depth.parse().unwrap_or(0);
-        //         if depth_int > 0 {
-        //             clone_pattern.push("--depth");
-        //             clone_pattern.push(depth);
-        //         }
-        //     }
-
-        //     clone_pattern.push(&clonefmt);
-
-        //     let res = Execute::exe("git", &clone_pattern);
-        //     if res.is_ok() {
-        //         println!("{}", Col::Green.print_col("Clone the repo well"))
-        //     } else if res.is_err() {
-        //         println!("{}", Col::Red.print_col("Clone the repo didn't go well"))
-        //     }
-        //}
     }
 
     /// Helper function to push changes to the remote repository.
